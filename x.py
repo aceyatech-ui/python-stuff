3ae81fd462be7331a660bd9bc3eaf8a91769e4fc
aceyatech-ui <aceya.tech@gmail.com>
2026-02-23 15:44:01 +0100

    Guess x

---
+++ x.py
@@ -0,0 +1,15 @@
+x = 12
+
+user_input = int(input("Guess the value of x: "))
+
+if user_input < x:
+    print("x is less than 12")
+elif user_input == x:
+    print("x is not equal to 12")
+elif user_input == x:
+    print("Right on the money")
+else:
+    print("x is greater than 12")
+
+print("The value of x is:", x)
+
